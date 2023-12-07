10e57f65acf01b29458bcb46ae32024a5aa02c57
YangHyoChan <yang_hc@assist-8.com>
2023-12-07 11:10:50 +0900

    파이보 프로젝트 기능구현 끝

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from pybo.views import base_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pybo/', include('pybo.urls')),
+    path('common/', include('common.urls')),
+    path('', base_views.index, name='index'),
 ]
