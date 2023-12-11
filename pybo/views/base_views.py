127066d82abfba0d1bf4bd56f112fbdda46c54cd
YangHyoChan <yang_hc@assist-8.com>
2023-12-11 13:51:54 +0900

    로깅설정2

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -4,12 +4,15 @@ from django.shortcuts import render, get_object_or_404
 from pybo.models import Question
 
 from django.db.models import Q, Count
+import logging
+logger = logging.getLogger('pybo')
 
 
 def index(request):
     """
     pybo 목록 출력
     """
+    logger.info("INFO 레벨로 출력")
     # 입력 파라미터
     page = request.GET.get('page', '1')  # 페이지
     kw = request.GET.get('kw', '')  # 검색어
