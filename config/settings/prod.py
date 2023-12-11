e0df7d3be0ca8365ed65d4013f3bae87de4d48d5
YangHyoChan <yang_hc@assist-8.com>
2023-12-11 15:14:47 +0900

    디비변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,14 @@ ALLOWED_HOSTS = ["52.79.107.37"]
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': 'Uwrvuzs)uP>oC56&dV8gfa>#aj;Dea~`',
+        'HOST': 'ls-22c264be22bc827757f101650c780eaa16114811.c3kvi9sgwvge.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
